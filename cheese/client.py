1c2fa569d7aed21041bd075838dce9318e7ea87a
Moe Myint Shein <moemyintshein@outlook.com>
2019-10-05 12:17:37 +0630

    tuple list

---
+++ cheese/client.py
@@ -0,0 +1,9 @@
+import cheese
+
+f = cheese.cheeseshop("Limburger", "It's very runny, sir.",
+           "It's really very, VERY runny, sir.",
+           shopkeeper="Michael Palin",
+           client="John Cleese",
+           sketch="Cheese Shop Sketch")
+
+print(f)
--- fibonacci/fib1.py
+++ fibonacci/fib1.py
@@ -1,16 +1,16 @@
-# import fib
+import fib
 
-# f = fib.fibo(500)
-# print(f)
+f = fib.fibo(500)
+print(f)
 
 # fib.fibo(1000)
 
 # f = fib.fib2
 # print(f(100))
 
-from fib import *
-fib(500)
+# from fib import *
+# fib(500)
 
 
-from fibo import fib as fibonacci
-fibonacci(500)
+# from fibo import fib as fibonacci
+# fibonacci(500)
